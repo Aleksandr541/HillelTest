9452656d3965e71354b379c8807835d8a5f48c19
Alexandr541 <adshvec@gmail.com>
2021-03-10 10:31:48 +0200

    Homework2

---
+++ src/com/hillel/lesson3/homrwork3/HomeworkScanner.java
@@ -0,0 +1,14 @@
+package com.hillel.lesson3.homrwork3;
+
+import java.util.Scanner;
+
+public class HomeworkScanner {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите что нибудь");
+        String string s.nextLine();
+        System.out.println("Вы ввели"+string);
+
+    }
+}
