0f1b7b9b5f5f28430d9880fff6b75c9cf62afe50
Alexandr541 <adshvec@gmail.com>
2021-03-10 12:15:00 +0200

    Homework2

---
+++ src/com/hillel/lesson3/homrwork3/HomeworkRandom.java
@@ -0,0 +1,17 @@
+package com.hillel.lesson3.homrwork3;
+
+import java.util.Random
+
+public class HomeworkRandom {
+    public static void main(String[] args) {
+
+        int a = Math.random();
+        int b = Math.random();
+        int c = Math.random();
+
+        double x =  Math.random();
+        double y = Math.random();
+        double z = Math.random();
+
+    }
+}
--- src/com/hillel/lesson3/homrwork3/HomeworkScanner.java
+++ src/com/hillel/lesson3/homrwork3/HomeworkScanner.java
@@ -6,9 +6,33 @@ public class HomeworkScanner {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Введите что нибудь");
-        String string s.nextLine();
-        System.out.println("Вы ввели"+string);
+
+        System.out.print("Введите Ваше имя:");
+        String FirstName = s.nextLine();
+
+        System.out.println("Введите Вашу фамилию:");
+        String LastName = s.nextLine();
+
+        System.out.print("Укажите сколько Вам полных лет:");
+        int age = s.nextInt();
+        s.nextLine();
+
+//        System.out.println("Укажите сколько Вам полных лет:");
+//        Integer.age(s.nextLine());
+
+        System.out.println("Введите Вашу почту для регистрации:");
+        String email = s.nextLine();
+
+        System.out.println("Напишите несколько слов о себе:");
+        String intrview = s.nextLine();
+
+        System.out.println("Введите Ваше имя:" + FirstName);
+        System.out.println("Введите Вашу фамилию:" + LastName);
+        System.out.println("Укажите сколько Вам полных лет:" + age);
+        System.out.println("Введите Вашу почту для регистрации:" + email);
+        System.out.println("Напишите несколько слов о себе:"+intrview);
+
+        s.close();
 
     }
 }
