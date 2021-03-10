2290d1f7556105c7038661396931da51e23447e0
Alexandr541 <adshvec@gmail.com>
2021-03-10 15:46:06 +0200

    Homework2 1

--- src/com/hillel/lesson3/homrwork3/lotereya.java
+++ src/com/hillel/lesson3/homrwork3/lotereya.java
@@ -1,4 +1,32 @@
 package com.hillel.lesson3.homrwork3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class lotereya {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("Введите количество игроков: ");
+        int n = in.nextInt();
+        System.out.printf("Поздравляю победил игрок №:");
+
+
+//       in.close();
+
+        Math.random();
+        int min = 1;
+        int max = n;
+        int diff = max - min;
+        java.util.Random Random = new Random();
+        int i = Random.nextInt(diff + 1);
+        i += min;
+        System.out.println(i);
+
+
+    }
 }
