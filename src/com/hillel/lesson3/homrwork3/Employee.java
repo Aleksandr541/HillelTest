7d86bc0dcb1a1e7cda8540b0eac5a83dc5657ea8
Alexandr541 <adshvec@gmail.com>
2021-03-13 09:54:21 +0200

    Homework3 1

--- src/com/hillel/lesson3/homrwork3/Employee.java
+++ src/com/hillel/lesson3/homrwork3/Employee.java
@@ -1,6 +1,27 @@
 package com.hillel.lesson3.homrwork3;
 
-public class Employee {
+import com.sun.deploy.cache.BaseLocalApplicationProperties;
 
+import java.lang.management.ManagementFactory;
+
+public class Employee {
+    public static void main(String[] args) {
+
+
+        String position = "position1";
+        Integer age1 = 10;
+        Double salary1 = new Double(0.0);
+
+        String positionStringPooled = "position2";
+        Integer age2 = 13;
+        Double salary2 = new Double(0.0);
+
+        String b = new String(position);
+        boolean comparedByValues = position.equals(b);
+        System.out.println(comparedByValues);
+        System.out.println(age1.equals(age2));
+
+        System.out.println(salary1.equals(salary2));
+    }
 
 }
